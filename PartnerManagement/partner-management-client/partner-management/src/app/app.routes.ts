ed33a7d884ba711c9e3c8c79b26265f8f8ad289b
Petar Luketić <petravitez92@gmail.com>
2025-05-04 16:35:18 +0200

    fix structure

--- PartnerManagement/partner-management-client/partner-management/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { PartnerListComponent } from './component/partner-list/partner-list.component';
-import { PartnerCreateComponent } from './component/partner-create/partner-create.component';
-
-export const routes: Routes = [
-    { path: '', component: PartnerListComponent },
-    { path: 'partners/create', component: PartnerCreateComponent },
-  ];
-  
